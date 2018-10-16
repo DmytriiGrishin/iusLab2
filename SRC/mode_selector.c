e72f34344d6e8b146c2b9fbd9e5eafc43e0c4c36
dmitriy <dmytriigrishin@gmail.com>
2018-10-16 11:59:41 +0300

    Fixed INT0

--- SRC/mode_selector.c
+++ SRC/mode_selector.c
@@ -1,10 +1,13 @@
 #include "mode_selector.h"
+#include "max.h"
 u8 mode;
 
 void SERIAL_COMMUNICATION_ISR (void) __interrupt (1);
 void initialize_mode_selector() {
 	IT0 = 1;  
 	EX0 = 1; 
+	unsigned char old_ena = read_max(ENA);
+	write_max(ENA, old_ena | 0x20);
 	SetVector(0x2003, (void *) SERIAL_COMMUNICATION_ISR); 
 }
 void SERIAL_COMMUNICATION_ISR( void ) __interrupt (1){
